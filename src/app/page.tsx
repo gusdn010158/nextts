25cacb6d1604bf26085e2d9789dfdee9568f7276
gusdn010158 <gusdn010158@gmail.com>
2025-01-15 01:21:45 +0900

    frvecel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,3 @@
-// 'use client';
-// import { useEffect, useState } from 'react';
-// import { Exhibition } from '@/redux/store';
-// import Item from './Item/Item.tsx';
-// import { getApiExhibitionList } from '../apis/index.ts';
-
-// import styles from '../app/main.module.css';
-
-// const Home: React.FC<Exhibition> = () => {
-//   const [exhibitionList, setExhibitionList] = useState<Exhibition[]>([]);
-
-//   useEffect(() => {
-//     getApiExhibitionList()
-//       .then((exhibitionList) => {
-//         setExhibitionList(exhibitionList);
-//       })
-//       .catch((error) => {
-//         console.error('API 호출 중 오류 발생:', error);
-//       });
-//   }, []);
-
-//   return (
-//     <>
-//       <div className={styles.main}>
-//         <div className={styles.maincomponent}>
-//           {exhibitionList.map((exhibition, index) => (
-//             <Item key={index} exhibition={exhibition} />
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-// export default Home;
 'use client';
 import { useEffect, useState } from 'react';
 import { Exhibition } from '@/redux/store'; // Exhibition 타입이 정의되어 있어야 합니다.
