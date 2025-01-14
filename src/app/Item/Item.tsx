8af0b3a6c70b0fcf3bb4dd2c8382e2ad52b616d4
gusdn010158 <gusdn010158@gmail.com>
2025-01-14 13:38:29 +0900

    reee

--- src/app/Item/Item.tsx
+++ src/app/Item/Item.tsx
@@ -1,42 +1,3 @@
-// import Link from 'next/link';
-// import styles from '../main.module.css';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { add, removeid, Exhibition } from '../../redux/store';
-// import { IoMdStar } from 'react-icons/io';
-// import { IoIosStarOutline } from 'react-icons/io';
-// import Itemcom from './Itemcom';
-
-// const Item: React.FC<{ exhibition: Exhibition }> = ({ exhibition }) => {
-//   const dispatch = useDispatch();
-//   const favorites = useSelector((state: Exhibition[]) => state);
-
-//   const isFavorite = favorites.some((fav: Exhibition) => fav.id === exhibition.id);
-//   //some()매서드는 주어진 function을 하나라도 통과하는 배열의 element가 있으면 true를 반환하고 그렇지 않으면 false를 반환
-
-//   const handleStarClick = () => {
-//     if (isFavorite) {
-//       dispatch(removeid(exhibition.id));
-//     } else {
-//       dispatch(add(exhibition));
-//     }
-//   };
-
-//   return (
-//     <div className={styles.component}>
-//       <Itemcom exhibition={exhibition} />
-//       <div className={styles.Itembottom}>
-//         <div className={styles.right} onClick={handleStarClick}>
-//           {isFavorite ? <IoMdStar /> : <IoIosStarOutline />}
-//         </div>
-//         <Link href={`/reservation/${exhibition.id}`} className={styles.button}>
-//           예매 하기
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Item;
 import Link from 'next/link';
 import styles from '../main.module.css';
 import { useDispatch, useSelector } from 'react-redux';
@@ -47,9 +8,10 @@ import Itemcom from './Itemcom';
 
 const Item: React.FC<{ exhibition: Exhibition }> = ({ exhibition }) => {
   const dispatch = useDispatch();
-  const favorites = useSelector((state: { favorites: Exhibition[] }) => state.favorites) || []; // 기본값 추가
+  const favorites = useSelector((state: Exhibition[]) => state);
 
   const isFavorite = favorites.some((fav: Exhibition) => fav.id === exhibition.id);
+  //some()매서드는 주어진 function을 하나라도 통과하는 배열의 element가 있으면 true를 반환하고 그렇지 않으면 false를 반환
 
   const handleStarClick = () => {
     if (isFavorite) {
@@ -75,3 +37,41 @@ const Item: React.FC<{ exhibition: Exhibition }> = ({ exhibition }) => {
 };
 
 export default Item;
+// import Link from 'next/link';
+// import styles from '../main.module.css';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { add, removeid, Exhibition } from '../../redux/store';
+// import { IoMdStar } from 'react-icons/io';
+// import { IoIosStarOutline } from 'react-icons/io';
+// import Itemcom from './Itemcom';
+
+// const Item: React.FC<{ exhibition: Exhibition }> = ({ exhibition }) => {
+//   const dispatch = useDispatch();
+//   const favorites = useSelector((state: { favorites: Exhibition[] }) => state.favorites) || []; // 기본값 추가
+
+//   const isFavorite = favorites.some((fav: Exhibition) => fav.id === exhibition.id);
+
+//   const handleStarClick = () => {
+//     if (isFavorite) {
+//       dispatch(removeid(exhibition.id));
+//     } else {
+//       dispatch(add(exhibition));
+//     }
+//   };
+
+//   return (
+//     <div className={styles.component}>
+//       <Itemcom exhibition={exhibition} />
+//       <div className={styles.Itembottom}>
+//         <div className={styles.right} onClick={handleStarClick}>
+//           {isFavorite ? <IoMdStar /> : <IoIosStarOutline />}
+//         </div>
+//         <Link href={`/reservation/${exhibition.id}`} className={styles.button}>
+//           예매 하기
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Item;
--- src/app/zzim/page.tsx
+++ src/app/zzim/page.tsx
@@ -1,32 +1,12 @@
-// 'use client';
-// import { useSelector } from 'react-redux';
-// import styles from '../main.module.css';
-// import { Exhibition } from '@/redux/store';
-
-// import Zzim from './Zzim';
-
-// const Zzom: React.FC<Exhibition> = () => {
-//   const favorites = useSelector((state: Exhibition[]) => state);
-
-//   return (
-//     <div className={styles.main}>
-//       {favorites.length === 0 ? (
-//         <div>찜 해놓은 전시회가 없습니다.</div>
-//       ) : (
-//         favorites.map((exhibition: Exhibition) => <Zzim exhibition={exhibition} />)
-//       )}
-//     </div>
-//   );
-// };
-// export default Zzom;
 'use client';
 import { useSelector } from 'react-redux';
 import styles from '../main.module.css';
 import { Exhibition } from '@/redux/store';
+
 import Zzim from './Zzim';
 
-const Zzom: React.FC = () => {
-  const favorites = useSelector((state: { favorites: Exhibition[] }) => state.favorites);
+const Zzom: React.FC<Exhibition> = () => {
+  const favorites = useSelector((state: Exhibition[]) => state);
 
   return (
     <div className={styles.main}>
@@ -40,5 +20,27 @@ const Zzom: React.FC = () => {
     </div>
   );
 };
-
 export default Zzom;
+// 'use client';
+// import { useSelector } from 'react-redux';
+// import styles from '../main.module.css';
+// import { Exhibition } from '@/redux/store';
+// import Zzim from './Zzim';
+
+// const Zzom: React.FC = () => {
+//   const favorites = useSelector((state: { favorites: Exhibition[] }) => state.favorites);
+
+//   return (
+//     <div className={styles.main}>
+//       {favorites.length === 0 ? (
+//         <div>찜 해놓은 전시회가 없습니다.</div>
+//       ) : (
+//         favorites.map((exhibition: Exhibition) => (
+//           <Zzim key={exhibition.id} exhibition={exhibition} />
+//         ))
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Zzom;
